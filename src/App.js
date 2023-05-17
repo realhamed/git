e7671a7e23abd26c5ef084cb8ec7601ff6018c58
hAmed <hamed.blacklock1992@gmail.com>
2023-05-17 11:02:48 +0330

    conflict in vs

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* hello this is a new test */}
-      {/* this is the second test */}
-{/* changes in github */}
+      {/* conflict in vs code */}
     </div>
   );
 }
